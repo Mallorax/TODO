39709dd142e7b86e04fb6543b190d86b16cc1159
mallorax <patryk733773@gmail.com>
2019-10-24 02:55:30 +0200

    configured data binding for task list item

--- app/src/main/java/pl/patrykzygo/todo/ui/BindingAdapters.kt
+++ app/src/main/java/pl/patrykzygo/todo/ui/BindingAdapters.kt
@@ -1,11 +1,21 @@
 package pl.patrykzygo.todo.ui
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import pl.patrykzygo.todo.domain.Task
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("taskList")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Task>?){
     val adapter = recyclerView.adapter as TasksListAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("task_date")
+fun bindTaskDate(textView: TextView, date: Date){
+    val format = SimpleDateFormat("dd.MM.yyyy HH:mm")
+    val dateString = format.format(date)
+    textView.text = dateString
 }
