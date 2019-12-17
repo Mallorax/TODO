120806135ffcc982050a60136d62c6f3a2947d2e
mallorax <patryk733773@gmail.com>
2019-12-17 23:06:47 +0100

    changed communication room -> repo -> vm -> ui, now it goes through livedata. Prieviously it was going throuugh plain objects and view wasn't notified when other fragment added data into db, so it basically fixed that problem.

--- app/src/main/java/pl/patrykzygo/todo/repository/TaskRepository.kt
+++ app/src/main/java/pl/patrykzygo/todo/repository/TaskRepository.kt
@@ -1,6 +1,7 @@
 package pl.patrykzygo.todo.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.flow.Flow
 import pl.patrykzygo.todo.database.TaskEntity
 import pl.patrykzygo.todo.domain.Task
@@ -8,8 +9,8 @@ import pl.patrykzygo.todo.domain.Task
 interface TaskRepository {
 
     suspend fun insertTask(vararg tasks: Task)
-    suspend fun receiveAllTasks(): List<Task>
-    suspend fun getTaskWithId(id: Long): Task
+    fun receiveAllTasks(): LiveData<List<Task>>
+    fun getTaskWithId(id: Long): LiveData<Task>
     suspend fun deleteSpecificTasks(vararg tasks: Task)
     suspend fun clearAllTasks()
 
