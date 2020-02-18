f709c1569b4ec8806b383091484a62f285d5b219
mallorax <patryk733773@gmail.com>
2020-02-18 20:15:03 +0100

    added alarm sound, it plays for too short though,, need to fix that and add option to dismiss it

--- app/src/main/java/pl/patrykzygo/todo/Services/AlarmReceiver.kt
+++ app/src/main/java/pl/patrykzygo/todo/Services/AlarmReceiver.kt
@@ -3,6 +3,8 @@ package pl.patrykzygo.todo.Services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.util.Log
 import android.widget.Toast
 
@@ -10,5 +12,8 @@ class AlarmReceiver: BroadcastReceiver (){
 
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "Alarm fired", Toast.LENGTH_LONG).show()
+        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        val mp = MediaPlayer.create(context, uri)
+        mp.start()
     }
 }
